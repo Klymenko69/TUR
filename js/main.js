93fc41ca90c33da4adddfea0b3270d18fd45a099
Klymenko69 <32891755+Klymenko69@users.noreply.github.com>
2020-03-23 20:17:08 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 function collapseMenu() {
     let frame = document.getElementById("pictures");
     let blur = document.getElementById("container-blur");
+    let close = document.getElementById("close");
     if (frame.style.display === "flex") {
-        frame.style.display = "none";
+        close = frame.style.display = "none";
         blur.style.filter = 'none';
     } else {
         frame.style.display = "flex";
